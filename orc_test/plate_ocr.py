5c01731d38c7455f669eb93e139ba6e222be79c5
khanh <khanhtq.b8084@st.usth.edu.vn>
2020-04-19 23:27:31 +0700

    fix some bug

--- orc_test/plate_ocr.py
+++ orc_test/plate_ocr.py
@@ -1,64 +1,72 @@
 import cv2
 import imutils
 import numpy as np
+import glob
 
-# Param
-max_size = 5000
-min_size = 900
+cv_img = []
+def detect_plate(img_path):
 
-# Load image
-img = cv2.imread('plate\P6040010.jpg', cv2.IMREAD_COLOR)
+    # Param
+    max_size = 5000
+    min_size = 900
 
-# Resize image
-img = cv2.resize(img, (620, 480))
+    # Load image
 
-# Edge detection
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grey scale
-gray = cv2.bilateralFilter(gray, 11, 17, 17)  # Blur to reduce noise
-edged = cv2.Canny(gray, 30, 200)  # Perform Edge detection
+    # Resize image
+    img = cv2.resize(img_path, (620, 480))
 
-# find contours in the edged image, keep only the largest
-# ones, and initialize our screen contour
-cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-screenCnt = None
+    # Edge detection
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grey scale
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)  # Blur to reduce noise
+    edged = cv2.Canny(gray, 30, 200)  # Perform Edge detection
 
-# loop over our contours
-for c in cnts:
-    # approximate the contour
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.05 * peri, True)
+    # find contours in the edged image, keep only the largest
+    # ones, and initialize our screen contour
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    screenCnt = None
 
-    # if our approximated contour has four points, then
-    # we can assume that we have found our screen
-    if len(approx) == 4 and max_size > cv2.contourArea(c) > min_size:
-        screenCnt = approx
-        break
+    # loop over our contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.05 * peri, True)
 
-if screenCnt is None:
-    detected = 0
-    print ("No plate detected")
-else:
-    detected = 1
+        # if our approximated contour has four points, then
+        # we can assume that we have found our screen
+        if len(approx) == 4 and max_size > cv2.contourArea(c) > min_size:
+            screenCnt = approx
+            break
 
-if detected == 1:
-    cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
+    if screenCnt is None:
+        detected = 0
+        print ("No plate detected: ")
+    else:
+        detected = 1
 
-    # Masking the part other than the number plate
-    mask = np.zeros(gray.shape, np.uint8)
-    new_image = cv2.drawContours(mask, [screenCnt], 0, 255, -1, )
-    new_image = cv2.bitwise_and(img, img, mask=mask)
+    if detected == 1:
+        cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
 
-    # Now crop
-    (x, y) = np.where(mask == 255)
-    (topx, topy) = (np.min(x), np.min(y))
-    (bottomx, bottomy) = (np.max(x), np.max(y))
-    Cropped = gray[topx:bottomx + 1, topy:bottomy + 1]
+        # Masking the part other than the number plate
+        mask = np.zeros(gray.shape, np.uint8)
+        new_image = cv2.drawContours(mask, [screenCnt], 0, 255, -1, )
+        new_image = cv2.bitwise_and(img, img, mask=mask)
 
-    # Display image
-    cv2.imshow('Input image', img)
-    cv2.imshow('License plate', Cropped)
+        # Now crop
+        (x, y) = np.where(mask == 255)
+        (topx, topy) = (np.min(x), np.min(y))
+        (bottomx, bottomy) = (np.max(x), np.max(y))
+        Cropped = gray[topx:bottomx + 1, topy:bottomy + 1]
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        # Display image
+        cv2.imshow('Input image', img)
+        cv2.imshow('License plate', Cropped)
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+for img in glob.glob("plate/*.jpg"):
+    n= cv2.imread(img,cv2.IMREAD_COLOR)
+    detect_plate(n)
+    print(img)
